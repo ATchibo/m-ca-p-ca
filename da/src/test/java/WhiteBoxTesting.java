81f86369bb8a006e1d8bcdffba97f39ccf6809c9
911-Cirstea-Andrei-Daniel <91746306+911-Cirstea-Andrei-Daniel@users.noreply.github.com>
2024-05-30 15:08:36 +0300

    Them cows

---
+++ da/src/test/java/WhiteBoxTesting.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.Main.FindPair;
+import static org.example.Main.InsertCow;
+import static org.junit.Assert.*;
+
+public class WhiteBoxTesting {
+    @Test
+    public void testInsertCow(){
+        List<Integer> animals = new ArrayList<>();
+        int sizeBeforeInsert = animals.size();
+        InsertCow(animals);
+        assertTrue(sizeBeforeInsert + 1 == animals.size());
+        InsertCow(animals);
+        assertTrue(sizeBeforeInsert + 2 == animals.size());
+    }
+}
